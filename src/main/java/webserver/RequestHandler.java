b6301f231bb61d2fb4b097108baada6a6b854743
Donghyun Kim <newdhkim0615@gmail.com>
2026-04-01 20:52:18 +0900

    요구사항 3: POST 방식으로 회원가입 하기
    - form.html 의 method를 post로 변경
    - POST 요청 처리를 위한 handlePost() 메서드 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import db.MemoryUserRepository;
 import db.Repository;
 import http.util.HttpRequestUtils;
+import http.util.IOUtils;
 import model.User;
 
 import java.io.*;
@@ -30,36 +31,18 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            // Tomcat 구현 1단계 - 요구사항 1: index.html 반환하기
             String startLine[] = br.readLine()
                     .split(" ");
+
+            // Tomcat 구현 1단계 - 요구사항 1: index.html 반환하기
             if (startLine[0].equals("GET")) {
-
-                // Tomcat 구현 1단계 - 요구사항 2: GET 방식으로 회원가입 하기
-                if (startLine[1].startsWith("/user/signup")) {
-                    // ?를 기준으로 쿼리스트링 분리 및 &를 기준으로 쿼리스트링 파싱
-                    String queryString = startLine[1].split("\\?")[1];
-                    Map<String, String> parameters = HttpRequestUtils.parseQueryParameter(queryString);
-
-                    Repository repository = MemoryUserRepository.getInstance();
-
-                    // memoryUserRepository에 사용자가 입력한 ID가 존재하지 않는 경우 새로 추가
-                    if ((repository.findUserById(parameters.get("userId"))) == null) {
-                        User newUser = new User(parameters.get("userId"), parameters.get("password"),
-                                parameters.get("name"), parameters.get("email"));
-                        repository.addUser(newUser);
-
-                        // 다시 index.html 화면 띄우기
-                        responseHtmlFile(dos, "/index.html");
-
-                        return;
-                    }
-
-                    // 사용자가 입력한 ID가 존재하는 경우
-                    // ...
-                }
-
                 responseHtmlFile(dos, startLine[1]);
+                return;
+            }
+
+            // Tomcat 구현 1단계 - 요구사항 3: POST 방식으로 회원가입 하기
+            if (startLine[0].equals("POST")) {
+                handlePost(dos, startLine[1], br);
             }
 
         } catch (IOException e) {
@@ -67,7 +50,7 @@ public class RequestHandler implements Runnable{
         }
     }
 
-    // 요청 URL에 해당하는 HTML 파일을 읽어 응답으로 반환하는 메서드
+    // GET 방식의 요청 URL에 해당하는 HTML 파일을 읽어 응답으로 반환하는 메서드
     private void responseHtmlFile(DataOutputStream dos, String url) throws IOException {
 
         String path = "./webapp";
@@ -78,6 +61,42 @@ public class RequestHandler implements Runnable{
         byte[] body = Files.readAllBytes(Paths.get(path));
         response200Header(dos, body.length);
         responseBody(dos, body);
+    }
+
+    // POST 방식 로직 처리 메서드
+    private void handlePost(DataOutputStream dos, String url, BufferedReader br) throws IOException {
+        // 회원가입 로직
+        if (url.equals("/user/signup")) {
+            String queryString;
+            int contentLength = 0;
+
+            // Header Line이 끝나는 공백 다음부터 body이므로 해당 줄까지 넘김
+            // + Content-Length 값 저장
+            while (!(queryString = br.readLine()).isEmpty()) {
+                if (queryString.split(": ")[0].equals("Content-Length")) {
+                    contentLength = Integer.parseInt(queryString.split(": ")[1]);
+                }
+            }
+
+            // IOUtils, HttpRequestUtils 활용해 쿼리스트링 파싱
+            queryString = IOUtils.readData(br, contentLength);
+            Map<String, String> parameters = HttpRequestUtils.parseQueryParameter(queryString);
+
+            // memoryUserRepository에 사용자가 입력한 ID가 존재하지 않는 경우 새로 추가
+            Repository repository = MemoryUserRepository.getInstance();
+
+            if ((repository.findUserById(parameters.get("userId"))) == null) {
+                User newUser = new User(parameters.get("userId"), parameters.get("password"),
+                        parameters.get("name"), parameters.get("email"));
+                repository.addUser(newUser);
+
+                // 다시 index.html 화면 띄우기
+                responseHtmlFile(dos, "/index.html");
+            }
+
+            // 사용자가 입력한 ID가 존재하는 경우
+            // ...
+        }
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
